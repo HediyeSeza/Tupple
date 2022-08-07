9fbf56a2fef8b5cb0ca2f41ae1308126e11a0ff7
sezahedi <sezahedi13@gmail.com>
2022-08-07 20:02:55 +0430

    Add project files.

---
+++ Tupple/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tupple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (String, int) t1 = ("hedi", 21);
+            Console.WriteLine($"Tupple Name {t1.Item1} and age {t1.Item2} .");
+            Console.ReadLine();
+        }
+    }
+}
